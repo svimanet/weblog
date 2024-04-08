b5a38fe138774e5ab3280b9a8d318ef210c464f6
svimanet <svimanet@pm.me>
2024-04-08 21:19:58 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), sitemap()],
+  site: "https://svimanet.github.io",
+  base: "weblog",
+});
